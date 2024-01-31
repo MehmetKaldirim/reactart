383d3133b0aaee1100266b30f6fbdd432b9cd383
Mehmet <mathdenizi@gmail.com>
2024-01-31 03:00:09 +0100

    tailwind css added

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -48,27 +48,52 @@ export default function AuthInputs() {
 
   const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+  let labelClassesPassword =
+    "block mb-2 text-xs font-bold tracking-wide text-stone-200 uppercase";
+  if (passwordNotValid) {
+    labelClassesPassword =
+      "block mb-2 text-xs font-bold tracking-wide text-red-400 uppercase";
+  }
+
+  let labelClassesEmail =
+    "block mb-2 text-xs font-bold tracking-wide text-stone-200 uppercase";
+  if (emailNotValid) {
+    labelClassesEmail =
+      "block mb-2 text-xs font-bold tracking-wide text-red-400 uppercase";
+  }
+
+  let inputPasswordClasses =
+    "w-full px-3 py-2 leading-tight  border rounded shadow";
+  if (passwordNotValid) {
+    inputPasswordClasses += "text-red-500 bg-red-100 border-red-300";
+  } else {
+    inputPasswordClasses += " text-gray-700 bg-stone-300";
+  }
+
+  let inputEmailClasses =
+    "w-full px-3 py-2 leading-tight  border rounded shadow";
+  if (emailNotValid) {
+    inputEmailClasses += "text-red-500 bg-red-100 border-red-300";
+  } else {
+    inputEmailClasses += " text-gray-700 bg-stone-300";
+  }
 
   return (
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
-            Email
-          </Label>
-          <Input
+          <label className={labelClassesEmail}>Email</label>
+          <input
             type="email"
-            className={emailNotValid ? "invalid" : undefined}
+            className={inputEmailClasses}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <Label className={`label ${passwordNotValid ? "invalid" : ""}`}>
-            Password
-          </Label>
-          <Input
+          <label className={labelClassesPassword}>Password</label>
+          <input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            className={inputPasswordClasses}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
@@ -76,10 +101,16 @@ export default function AuthInputs() {
         </p>
       </ControlContainer>
       <div className="actions">
-        <button type="button" className="text-button">
+        <button
+          type="button"
+          className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500"
+        >
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <button
+          className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500"
+          onClick={handleLogin}
+        >
           Sign In
         </button>
       </div>
